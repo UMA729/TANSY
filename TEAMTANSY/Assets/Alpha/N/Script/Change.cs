5174eb991a7fcb5e43f19f609b96fa09900df3be
nakaya <h1202419010@hiratagakuen.onmicrosoft.com>
2024-11-28 12:36:02 +0900

    ボスの技を1つ完成

--- TEAMTANSY/Assets/Alpha/N/Script/Change.cs
+++ TEAMTANSY/Assets/Alpha/N/Script/Change.cs
@@ -5,9 +5,10 @@ using UnityEngine.SceneManagement;      //シーンの切り替えに必要
 
 public class Change : MonoBehaviour
 {
+    public string sceneName;
     // ボタンがクリックされたときにシーンを変更
     public void LoadSceneByButton()
     {
-        SceneManager.LoadScene("stage1");
+        SceneManager.LoadScene(sceneName);
     }
 }
