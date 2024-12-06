0239fda61822815978d7a9c2359cd791ae5bdf9c
nakaya <h1202419010@hiratagakuen.onmicrosoft.com>
2024-12-06 10:33:27 +0900

    変更

---
+++ TEAMTANSY/Assets/Alpha/N/Script/FadeScript.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class FadeScript : MonoBehaviour
+{
+    public GameObject Panelfade;//フェードパネルの取得
+    Image fadealpha;//フェードパネルのイメージ取得変数
+    private float alpha;//パネルのalpha値取得
+    private bool fadeout;//フェードアウトのフラグ変数
+    public string sceneName;//移動先のシーン名
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fadealpha = Panelfade.GetComponent<Image>();
+        alpha = fadealpha.color.a;
+        fadeout = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            if (fadeout == true)
+            {
+                Fadeout();
+                Debug.Log("フェードアーーーーーーウト");
+            }
+        }
+    }
+
+    void Fadeout()
+    {
+        Debug.Log("アムロいきまぁーーーーす");
+        alpha += 0.001f;
+        fadealpha.color = new Color(0, 0, 0, alpha);
+        if(alpha >= 1)
+        {
+            fadeout = false;
+            Debug.Log("移動じゃーーい");
+            //SceneManager.LoadScene(sceneName);
+        }
+        
+    }
+}
