dbce2272787144979e1526ab6e610f4b6d514d3f
DESKTOP-M30FNO0\GA1A <h1202419009@hiratagakuen.onmicrosoft.com>
2024-12-23 15:09:25 +0900

    アタッチのし忘れで起きていたエラーを治した

--- TEAMTANSY/Assets/Alpha/K/ScriptF/ItemScript/ItemBox.cs
+++ TEAMTANSY/Assets/Alpha/K/ScriptF/ItemScript/ItemBox.cs
@@ -8,15 +8,13 @@ public class ItemBox : MonoBehaviour
     public GameObject itemPrefab; //出てくるアイテムのプレハブ
     public bool isClosed = true; //true= しまっているfalse= 開いている
     public int arrangeId = 0;    //配置の識別に使う
-    public GameObject gettext1;
-    public GameObject gettext2;
+    public GameObject gettext;
     public GameObject ExplaTorch;
     private HPController HP;
     // Start is called before the first frame update
     void Start()
     {
-        gettext1.SetActive(false);
-        gettext2.SetActive(false);
+        gettext.SetActive(false);
         ExplaTorch.SetActive(false);
     }
 
@@ -38,9 +36,8 @@ public class ItemBox : MonoBehaviour
                 //アイテムをプレハブから作る
                 Instantiate(itemPrefab, transform.position, Quaternion.identity);
             }
-            gettext1.SetActive(true);
-            gettext2.SetActive(true);
-            Destroy(gettext1, 1f);
+            gettext.SetActive(true);
+            Destroy(gettext, 1f);
             ExplaTorch.SetActive(true);
         }
     }
