873e4e38ae383a0a9cc30fb2b23bc7b7d94ada02
DESKTOP-M30FNO0\GA1A <h1202419009@hiratagakuen.onmicrosoft.com>
2025-01-16 09:53:18 +0900

    試し2

--- TEAMTANSY/Assets/Alpha/K/ScriptF/ItemScript/BaffIcon.cs
+++ TEAMTANSY/Assets/Alpha/K/ScriptF/ItemScript/BaffIcon.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BaffIcon : MonoBehaviour
 {
+    [SerializeField]
+    private List<RectTransform> BuffandKey_Icon_List = new List<RectTransform>();
+    public RectTransform BuffandKey_Icon_Panel;
+    public RectTransform BuffandKey_Icon_base;
+    public Sprite[] Icon_BuffandKey = new Sprite[4];
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +20,16 @@ public class BaffIcon : MonoBehaviour
     void Update()
     {
         
+    }
+    public void Set_BuffandKey_Icon(bool Itemcon,int ID)
+    {
+        //アイコンをパネルの子にする
+        var icon = Instantiate(BuffandKey_Icon_base, BuffandKey_Icon_Panel);
+        
+        //アイコン画像を入れる
+        if (Itemcon)
+        {
+            icon.GetComponent<Image>().sprite = Icon_BuffandKey[ID];
+        }
     }
 }
