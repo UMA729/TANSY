4aa9c45379d46e74c7a08fd911e4e93ea2fa4edf
DESKTOP-M30FNO0\GA1A <h1202419009@hiratagakuen.onmicrosoft.com>
2025-01-21 11:38:41 +0900

    シーン切り替え時の音を追加

--- TEAMTANSY/Assets/Alpha/N/Script/Change.cs
+++ TEAMTANSY/Assets/Alpha/N/Script/Change.cs
@@ -6,10 +6,35 @@ using UnityEngine.SceneManagement;      //シーンの切り替えに必要
 public class Change : MonoBehaviour
 {
     public string sceneName;
+    float time = 0.0f;
+    bool nextscene = false;
+
     // ボタンがクリックされたときにシーンを変更
-    public void LoadSceneByButton()
+    private void Update()
     {
-        SceneManager.LoadScene(sceneName);
-        Time.timeScale = 1;
+        if(nextscene)
+        LoadScene();
+    }
+    public void LoadScene()
+    {
+        float NSdur = 0.1f;
+
+        time += Time.deltaTime;
+        //タイムが計測変数の値を超えると次のシーンへ
+        if (time >= NSdur)
+        {
+            SceneManager.LoadScene(sceneName);
+            Time.timeScale = 1;
+
+            time = 0.0f;
+            nextscene = false;
+        }
+    }
+    // ボタンがクリックされたとき
+    public void ClickButton()
+    {
+        //nextsceneがfalseでない場合trueにする
+        if(!nextscene)
+        nextscene = true;
     }
 }
